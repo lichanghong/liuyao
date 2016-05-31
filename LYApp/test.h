534de90f8aec97a97d0b2761d7f56ba43fb01c76
lichanghong <1211054926@qq.com>
2016-05-31 22:47:35 +0800

    数据处理+ 六神完成

---
+++ LYApp/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  LYApp
+//
+//  Created by 李长鸿 on 5/31/16.
+//  Copyright © 2016 lichanghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UITableViewCell
+
+@end
---
+++ LYApp/test.m
@@ -0,0 +1,23 @@
+//
+//  test.m
+//  LYApp
+//
+//  Created by 李长鸿 on 5/31/16.
+//  Copyright © 2016 lichanghong. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
