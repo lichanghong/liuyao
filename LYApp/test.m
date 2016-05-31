b203f9202371f8770bb804a7da0857bb89b443f1
lichanghong <1211054926@qq.com>
2016-05-31 22:47:44 +0800

    数据处理+ 六神完成

--- LYApp/test.m
+++ LYApp/test.m
@@ -1,23 +1,18 @@
-//
-//  test.m
-//  LYApp
-//
-//  Created by 李长鸿 on 5/31/16.
-//  Copyright © 2016 lichanghong. All rights reserved.
-//
-
-#import "test.h"
-
-@implementation test
-
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
-@end
+if ([tiangan isEqualToString:"甲"]||[tiangan isEqualToString:"乙"]){
+    //        result =[NSArray arrayWithObjects:"龙","雀","勾","蛇","虎","武", nil];
+    //    }
+    //        if ([tiangan isEqualToString:"丙"]||[tiangan isEqualToString:"丁"]){
+    //        result =[NSArray arrayWithObjects:"雀","勾","蛇","虎","武","龙", nil];
+    //    }
+    //        if ([tiangan isEqualToString:"戊"]){
+    //        result =[NSArray arrayWithObjects:"勾","蛇","虎","武","龙","雀", nil];
+    //    }
+    //        if ([tiangan isEqualToString:"己"]){
+    //        result =[NSArray arrayWithObjects:"蛇","虎","武","龙","雀","勾", nil];
+    //    }
+    //        if ([tiangan isEqualToString:"庚"]||[tiangan isEqualToString:"辛"]){
+    //        result =[NSArray arrayWithObjects:"虎","武","龙","雀","陈","蛇", nil];
+    //    }
+    //        if ([tiangan isEqualToString:"壬"]||[tiangan isEqualToString:"癸"]){
+    //        result =[NSArray arrayWithObjects:"武","龙","雀","陈","蛇","虎", nil];
+    //    }};
