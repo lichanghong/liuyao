3b3a6fb9abddfe7168a9df0e1237b0f902a9754a
Your Name <you@example.com>
2016-06-22 10:02:36 +0800

    ui

--- LYApp/LYClientViewController.m
+++ LYApp/LYClientViewController.m
@@ -20,6 +20,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleAction:)];
+    tap.numberOfTapsRequired = 1;
+    tap.numberOfTouchesRequired =1;
+    [self.view addGestureRecognizer: tap];
     _qiguaAlertView = [QiGuaAlertView createView];
      // Do any additional setup after loading the view.
 }
@@ -31,6 +35,10 @@
 
 
 - (IBAction)handleAction:(id)sender {
+    if ([sender isKindOfClass: [UIGestureRecognizer class]]) {
+        [self.view endEditing:YES];
+    }
+    else
     [_qiguaAlertView showInView:self.view];
     
 }
